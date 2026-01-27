d5e34ac913421d6968dedfbdb20bf39595ef8b21
Narasimhareddy-dev <posttonarsi30@gmail.com>
2026-01-27 16:51:03 +0530

    Even and Odd

---
+++ Even.js
@@ -0,0 +1,10 @@
+function even_odd(num) {
+    if (num % 2 == 0) {
+        console.log(num, "Is a Even Number.");
+    }
+    else {
+        console.log(num, "Is a Odd Number.");
+    }
+}
+even_odd(12);
+even_odd(7);
