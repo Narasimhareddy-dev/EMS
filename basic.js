7b6ac88f07bc87e3e0aab6c6f36ddf23f59b81c4
Narasimhareddy-dev <posttonarsi30@gmail.com>
2026-01-27 16:57:21 +0530

    Basics of js

---
+++ basic.js
@@ -0,0 +1,18 @@
+console.log("Empliz innovations pvt ltd")
+var a = 12;
+let b = 23;
+console.log("Printing a variable", a);
+console.log("Printing b variable", b);
+console.log("Addition: ",a+b)
+let s = "I am Narasimhareddy";
+console.log(s);
+let bool = true;
+console.log("Boolean value: ", bool);
+let n = null;
+console.log("Null value: ", n);
+let u = undefined;
+console.log("Undefined value: ", u);
+let num = 12345;
+console.log("Number value: ", num);
+let bigInt = 123456789056789n;
+console.log("BigInt value: ", bigInt);
