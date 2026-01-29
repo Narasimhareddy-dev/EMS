ec31203ed643639baefc2b0b41c922b6dbe4e5b6
Narasimhareddy-dev <posttonarsi30@gmail.com>
2026-01-29 16:46:37 +0530

    Sum of Elements in a given array.

---
+++ Sumofarray.js
@@ -0,0 +1,6 @@
+let numbers = [12,13,12,14,15,16,17,18,19,20,21,22,23,24,25];
+sum = 0;
+for (let i = 0; i<numbers.length; i++){
+    sum = sum + numbers[i];
+}
+console.log("Sum of array elements is:", sum);
